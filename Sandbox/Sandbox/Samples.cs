1f822ffe35fef41f90765706e88c52344c228e1c
electricessence <5899455+electricessence@users.noreply.github.com>
2021-10-09 23:39:47 -0700

    Added package verification.

---
+++ Sandbox/Sandbox/Samples.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sandbox
+{
+	public static class Samples
+	{
+		public static bool BooleanAnd(bool a, bool b)
+			=> a & b;
+
+		public static int IntAnd(int a, int b)
+			=> a & b;
+
+		public static bool BooleanOr(bool a, bool b)
+			=> a | b;
+
+		public static int IntOr(int a, int b)
+			=> a | b;
+	}
+}
